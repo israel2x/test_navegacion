ee920b48f21875e54df7991b39fc266da50265b2
Instituto Superior Tecnológico La Troncal <istltdev@istlatroncal.edu.ec>
2026-07-15 20:58:41 -0500

    agregue el textbuton

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: TextButton(
+            onPressed: () {},
+            child: const Text('Primera Pantalla'),
+          ),
         ),
       ),
     );
